4ad39f8e96ea9822ce11a1ef2ac5ab15f2ef8f66
Anastasia Art <art-nastya@inbox.ru>
2019-04-10 10:29:59 +0300

    add style

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**"
+    "source/js/**/*.min.js"
   ], {
     base: "source"
   })
--- source/js/form-validate.js
+++ source/js/form-validate.js
@@ -1,15 +1,10 @@
 const modal = document.querySelector('.modal-error');
 const btnModal = document.querySelector('.modal-error__btn');
+const myform = document.getElementById("contact-form");
+let modalText = document.querySelector('.modal-error__text');
 
-var template_params = {
-   "name": "name_value",
-   "email": "email_value",
-   "tel": "tel_value",
-   "question": "question_value"
-}
-
-var service_id = "default_service";
-var template_id = "contact_form";
+const service_id = "default_service";
+const template_id = "contact_form";
 
 modal.classList.remove('modal-error--visible');
 
@@ -17,36 +12,43 @@ document.addEventListener('click', function(event) {
     var isClickInside = modal.contains(event.target);
     if (!isClickInside) {
         modal.classList.remove('modal-error--visible');
+        modalText.innerText='';
     }
 });
 
 document.querySelector('.form-feedback').addEventListener('submit', function(e) {
     let inputs = document.querySelectorAll('.input-js'), result, i;
 
+    e.preventDefault();
     for(i = 0; i < inputs.length; i++) {
         if(inputs[i].value) {
             result = true;
             break;
-            console.log(1);
         }
     }
     if(!result) {
         e.preventDefault();
-        modal.classList.add('modal-error--visible');
-        btnModal.addEventListener('click', modalClose);
+        openModal("Пожалуйста, укажите ваш Телефон или E-mail.");
         inputs[0].focus();
     }
     if(result) {
-        emailjs.send(service_id, template_id, template_params);
-    //     emailjs.sendForm('contact_service', 'contact_template', '.form-feedback')
-    //     .then(function(response) {
-    //         console.log('SUCCESS!', response.status, response.text);
-    //     }, function(error) {
-    //         console.log('FAILED...', error);
-    //     });
+        emailjs.sendForm(service_id, template_id, myform)
+        .then(function(response) {
+            openModal("Форма отправлена! В ближайшее время мы с вами свяжемся.");
+            myform.reset();
+        }, function(error) {
+            openModal("Что-то пошло не так, отправте форму еще раз.");
+        });
     }
 });
 
 function modalClose() {
     modal.classList.remove('modal-error--visible');
+    modalText.innerText='';
+}
+
+function openModal(text) {
+    modalText.innerText=text
+    modal.classList.add('modal-error--visible');
+    btnModal.addEventListener('click', modalClose);
 }
